d682829b26e03268fdd10f88217e41e19984c0eb
Patrick Evers <patrickevers2@web.de>
2020-03-03 11:45:18 +0100

    added multiply & divide function

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
+var input1 = document.getElementById('number1');
+var input2 = document.getElementById('number2');
+var number1 = 0;
+var number2 = 0;
+
 document.getElementById('addBtn').addEventListener('click', ()=>{
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-    var number1 = 0;
-    var number2 = 0;
 
-    if(!inputIsEmpty(input1)){
-        number1 = parseFloat(input1.value, 10);
-    }
-
-    if(!inputIsEmpty(input2)){
-        number2 = parseFloat(input2.value, 10);
-    }
+    setValues(input1, input2);
 
     var result = add(number1, number2);
 
-    var res = document.getElementById('result');
-    res.textContent = result;
+    showResult(result);
 })
 
 document.getElementById('subtractBtn').addEventListener('click', ()=>{
-    var input1 = document.getElementById('number1');
-    var input2 = document.getElementById('number2');
-    var number1 = 0;
-    var number2 = 0;
 
-    if(!inputIsEmpty(input1)){
-        number1 = parseFloat(input1.value, 10);
-    }
-
-    if(!inputIsEmpty(input2)){
-        number2 = parseFloat(input2.value, 10);
-    }
+    setValues(input1, input2);
 
     var result = subtract(number1, number2);
 
-    var res = document.getElementById('result');
-    res.textContent = result;
+    showResult(result);
+})
+
+document.getElementById('multiplyBtn').addEventListener('click', ()=>{
+
+    setValues(input1, input2);
+
+    var result = multiply(number1, number2);
+
+    showResult(result);
+})
+
+document.getElementById('divideBtn').addEventListener('click', ()=>{
+
+    setValues(input1, input2);
+
+    var result = divide(number1, number2);
+
+    showResult(result);
 })
 
 function add(number1, number2){
@@ -58,6 +59,31 @@ function subtract(number1, number2){
     else{
         return result.toFixed(countDecimals(number1,number2));
     }
+}
+
+function multiply(number1, number2){
+    var result = number1 * number2;
+    return result;
+}
+
+function divide(number1, number2){
+    var result = number1 / number2;
+    return result;
+}
+
+function setValues(input1, input2){
+    if(!inputIsEmpty(input1)){
+        number1 = parseFloat(input1.value, 10);
+    }
+
+    if(!inputIsEmpty(input2)){
+        number2 = parseFloat(input2.value, 10);
+    }
+}
+
+function showResult(result){
+    var res = document.getElementById('result');
+    res.textContent = result;
 }
 
 //Gibt die höchste Anzahl an Nachkommastellen für das Runden zurück.
